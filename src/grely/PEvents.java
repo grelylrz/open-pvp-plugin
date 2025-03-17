c571bbc456afe9b623546b60ae9fc3a8ebff5759
grelkalrz <182034229+grelylrz@users.noreply.github.com>
2025-03-17 23:05:19 +0500

    why

--- src/grely/PEvents.java
+++ src/grely/PEvents.java
@@ -2,6 +2,7 @@ package grely;
 
 import arc.Events;
 import arc.graphics.Color;
+import arc.util.Log;
 import mindustry.content.Blocks;
 import mindustry.content.Fx;
 import mindustry.game.EventType;
@@ -12,10 +13,14 @@ import mindustry.world.Tile;
 public class PEvents {
     public static void initEvents() {
         Events.on(EventType.BlockBuildEndEvent.class, e -> {
-            if(e.tile == null /*How?*/) return;
+            if(e.tile == null /*How?*/) {
+                Log.debug("Tile is null");
+                return;
+            }
             Tile t = e.tile;
             if(e.team != Team.derelict && !e.breaking && e.tile.block() == Blocks.vault /*ТЗ*/){
                 Call.effect(Fx.blockCrash, t.x*8, t.y*8, 1, Color.white);
+                t.setNet(Blocks.coreShard, e.team, 1);
             }
         });
     }
